ae27dddc8458caea9ba845c54cd003424e990fa9
cjang <cjang@student.42seoul.kr>
2021-10-10 16:01:56 +0900

    test_001

---
+++ matzip_sns/matzip_rest_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MatzipRestApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'matzip_rest_api'
---
+++ matzip_sns/matzip_rest_api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Evaluate(models.Model):
+	store = models.CharField(max_length=64)
+	star = models.CharField(max_length=5)
+	comment = models.CharField(max_length=256)
+	adreess = models.CharField(max_length=128)
+	time = models.DateTimeField()
