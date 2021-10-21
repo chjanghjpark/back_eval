fbf0a703e8282c090d609fb92a38d977d2844eff
loiipa <jch0apple@gmail.com>
2021-10-22 00:52:28 +0900

    delete prev bash

--- 1_install_env.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 -m venv mat_env
-source ./mat_env/bin/activate
-pip install -r requirements.txt
-pip list
--- 2_start_env.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source ./mat_env/bin/activate
-python3 ./matzip_sns/manage.py runserver
