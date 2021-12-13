f02542717c2d8d1b4c53ca0804b9f002f5c8fba3
cjang <cjang@student.42seoul.kr>
2021-12-13 11:39:23 +0900

    * 게시글 수정, 삭제 기능 구현 * 일부 예외사항 개선 필요 * 함수 정리 필요 * 토큰 발급부분은 오늘 저녁에 구현예정

--- matzip_sns/matzip_rest_api/views/evaluate.py
+++ matzip_sns/matzip_rest_api/views/evaluate.py
@@ -37,3 +37,45 @@ class EvaluateView(APIView):
 
 		eval = serializers.serialize("json", Evaluate.objects.filter(user=eval_user))
 		return JsonResponse({'eval': eval, 'message': 'success'}, status=200)
+
+	def put(self, request):
+		encoded_jwt = request.headers.get('Authorization', None)
+		decoded_jwt = validate_token(encoded_jwt)
+		user_id = decoded_jwt['user_id']
+		user_nickname = decoded_jwt['nickname']
+
+		eval_user = User.objects.get(username=user_id, password=user_id, last_name=user_nickname)
+
+		body = json.loads(request.body.decode('utf-8'))
+		eval_pk = body['pk']
+		eval_store = body['store']
+		eval_star = body['star']
+
+		# 다른 유저의 토큰으로 해당 유저의 글을 수정할 수 없음.
+		eval_obj = Evaluate.objects.get(id=eval_pk)
+		if (eval_user.username != str(eval_obj.user)):
+			return (JsonResponse({'message': 'not match user to eval'}, status=400))
+
+		eval_obj.store = eval_store
+		eval_obj.star = eval_star
+		eval_obj.save()
+
+		eval = serializers.serialize("json", Evaluate.objects.filter(pk = eval_pk))
+		return JsonResponse({'eval': eval, 'message': 'success'}, status=200)
+
+	def delete(self, request):
+		encoded_jwt = request.headers.get('Authorization', None)
+		decoded_jwt = validate_token(encoded_jwt)
+		user_id = decoded_jwt['user_id']
+		user_nickname = decoded_jwt['nickname']
+
+		eval_user = User.objects.get(username=user_id, password=user_id, last_name=user_nickname)
+
+		body = json.loads(request.body.decode('utf-8'))
+		eval_pk = body['pk']
+
+		eval_obj = Evaluate.objects.get(id=eval_pk)
+		if (eval_user.username != str(eval_obj.user)):
+			return (JsonResponse({'message': 'not match user to eval'}, status=400))
+		eval_obj.delete()
+		return JsonResponse({'message': 'success'}, status=200)
