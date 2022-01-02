1da67020699171a4fb8a87225a3e42c404d0b6d0
cjang <cjang@student.42seoul.kr>
2022-01-02 19:40:58 +0900

    Eval Post 수정 * content 추가 * 기타 에러처리 추가

--- matzip_sns/matzip_rest_api/views/evaluate.py
+++ matzip_sns/matzip_rest_api/views/evaluate.py
@@ -8,10 +8,15 @@ from django.contrib.auth.models import User
 from matzip_rest_api.models.models import Evaluate, Store
 from django.http import JsonResponse
 from django.core import serializers
+from django.core.exceptions import ValidationError
 
 class NotMatchUserEval(Exception):
 	def __init__(self):
 		super().__init__('NotMatchUserEval')
+
+class NotMatchAccessToken(Exception):
+	def __init__(self):
+		super().__init__('NotMatchAccessToken')
 
 @method_decorator(csrf_exempt, name='dispatch')
 class EvaluateView(APIView):
@@ -21,22 +26,23 @@ class EvaluateView(APIView):
 			encoded_jwt = request.headers.get('Authorization', None)
 			decoded_jwt = validate_token(encoded_jwt)
 			if (decoded_jwt['token_type'] != "access_token"):
-				raise ValueError
+				raise NotMatchAccessToken
 			body = json.loads(request.body.decode('utf-8'))
 
 			user = User.objects.get(username=decoded_jwt['user_id'], last_name=decoded_jwt['nickname'])
 			store, store_flag = Store.objects.get_or_create(store_name=body['store_name'], address=body['address'], \
 				latitude=body['latitude'], longitude=body['longitude'], store_type=body['store_type'])
-			eval = Evaluate.objects.create(user=user, store=store, star=int(body['star']), \
+			eval = Evaluate.objects.create(user=user, store=store, star=int(body['star']), content=body['content'], \
 				invited_date=body['invited_date'], open_close=body['open_close'])
-
-			return JsonResponse({'message': 'success'}, status=200)
+				
+			eval_return = serializers.serialize("json", Evaluate.objects.filter(pk=eval.pk))
+			return JsonResponse({'eval': eval_return, 'message': 'success'}, status=200)
 
 		# 토큰에 원하는 값이 없을 경우
 		except TypeError:
 			return (JsonResponse({'message': 'TOKEN_INFO ERROR'}, status=400))
 		# token_type이 accss_token 아닌경우 , token body 타입이 js 아닐때
-		except ValueError:
+		except NotMatchAccessToken:
 			return (JsonResponse({'message': 'TOKEN_TYPE ERROR'}, status=400))
 		# body - info not vaild
 		except KeyError:
@@ -44,6 +50,10 @@ class EvaluateView(APIView):
 		# user - not exist
 		except User.DoesNotExist:
 			return (JsonResponse({'message': 'USER REQUITED'}, status=400))
+		except ValidationError:
+			return (JsonResponse({'message': 'ValidationError'}, status=400))
+		except json.decoder.JSONDecodeError:
+			return (JsonResponse({'message': 'JSONDecodeError'}, status=400))
 
 
 	def get(self, request):
@@ -51,7 +61,7 @@ class EvaluateView(APIView):
 			encoded_jwt = request.headers.get('Authorization', None)
 			decoded_jwt = validate_token(encoded_jwt)
 			if (decoded_jwt['token_type'] != "access_token"):
-				raise ValueError
+				raise NotMatchAccessToken
 
 			user = User.objects.get(username=decoded_jwt['user_id'], last_name=decoded_jwt['nickname'])
 			eval = serializers.serialize("json", Evaluate.objects.filter(user=user))
@@ -62,7 +72,7 @@ class EvaluateView(APIView):
 		except TypeError:
 			return (JsonResponse({'message': 'TOKEN_INFO ERROR'}, status=400))
 		# token_type이 accss_token 아닌경우 , token body 타입이 js 아닐때
-		except ValueError:
+		except NotMatchAccessToken:
 			return (JsonResponse({'message': 'TOKEN_TYPE ERROR'}, status=400))
 		# user - not exist
 		except User.DoesNotExist:
@@ -73,7 +83,7 @@ class EvaluateView(APIView):
 			encoded_jwt = request.headers.get('Authorization', None)
 			decoded_jwt = validate_token(encoded_jwt)
 			if (decoded_jwt['token_type'] != "access_token"):
-				raise ValueError
+				raise NotMatchAccessToken
 
 			body = json.loads(request.body.decode('utf-8'))
 
@@ -88,6 +98,7 @@ class EvaluateView(APIView):
 			eval.star = int(body['star'])
 			eval.invited_date=body['invited_date']
 			eval.open_close=body['open_close']
+			eval.content=body['content']
 			eval.save()
 
 			eval = serializers.serialize("json", Evaluate.objects.filter(pk=body['pk']))
@@ -97,7 +108,7 @@ class EvaluateView(APIView):
 		except TypeError:
 			return (JsonResponse({'message': 'TOKEN_INFO ERROR'}, status=400))
 		# token_type이 accss_token 아닌경우 , token body 타입이 js 아닐때
-		except ValueError:
+		except NotMatchAccessToken:
 			return (JsonResponse({'message': 'TOKEN_TYPE ERROR'}, status=400))
 		# body - info not vaild
 		except KeyError:
@@ -112,13 +123,17 @@ class EvaluateView(APIView):
 		# 글쓴이랑 실제 유저랑 다른경우
 		except NotMatchUserEval:
 			return (JsonResponse({'message': 'not match user to eval'}, status=400))
+		except ValidationError:
+			return (JsonResponse({'message': 'ValidationError'}, status=400))
+		except json.decoder.JSONDecodeError:
+			return (JsonResponse({'message': 'JSONDecodeError'}, status=400))
 
 	def delete(self, request):
 		try:
 			encoded_jwt = request.headers.get('Authorization', None)
 			decoded_jwt = validate_token(encoded_jwt)
 			if (decoded_jwt['token_type'] != "access_token"):
-				raise ValueError
+				raise NotMatchAccessToken
 
 			body = json.loads(request.body.decode('utf-8'))
 
@@ -135,7 +150,7 @@ class EvaluateView(APIView):
 		except TypeError:
 			return (JsonResponse({'message': 'TOKEN_INFO ERROR'}, status=400))
 		# token_type이 accss_token 아닌경우 , token body 타입이 js 아닐때
-		except ValueError:
+		except NotMatchAccessToken:
 			return (JsonResponse({'message': 'TOKEN_TYPE ERROR'}, status=400))
 		# body - info not vaild
 		except KeyError:
